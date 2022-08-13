d771bb088be61423a3f08f20b9920af27e919253
Rayan-Aguiar <rayansiqueira@gmail.com>
2022-08-13 16:51:10 -0300

    mostrando saldo do usuario

--- areaprivada.php
+++ areaprivada.php
@@ -24,14 +24,7 @@ if (!isset($_SESSION['ID'])) {
 
 <body>
 
-    <?php
-    $u->conectar("projeto-pagamento", "localhost", "root", "");
-    $id = $_SESSION['ID'];
-    $sql = $pdo->prepare("SELECT CPF, nome, email FROM cliente WHERE CPF = :id ");
-    $sql->bindValue(":id", $id);
-    $sql->execute();
-    ?>
-    
+
     <div class="main-container">
         <h2>Seja bem-vindo(a)!</h2>
         <div class="bottons">
@@ -39,6 +32,28 @@ if (!isset($_SESSION['ID'])) {
             <a href="#">Transferir Saldo</a>
             <a href="Sair.php">Sair</a>
         </div>
+    </div>
+
+    <?php
+    $u->conectar("projeto-pagamento", "localhost", "root", "");
+    $id = $_SESSION['ID'];
+    $sql = $pdo->prepare("SELECT ID, CPF, nome, email, saldo FROM cliente WHERE ID = :id ");
+    $sql->bindValue(":id", $id);
+    $sql->execute();
+    ?>
+
+    <div class="dados-user">
+        <?php
+        while ($row_user = $sql->fetch(PDO::FETCH_ASSOC)) {
+        ?>
+            <div class="container-user">
+                <?php
+                echo "Nome: " . $row_user['nome']; ?><br><?php
+                echo "Email: " . $row_user['email']; ?><br><?php
+                echo "Saldo: R$ " . number_format($row_user['saldo'] , 2 , ',', '.'); ?><br><?php
+        }
+        ?>
+            </div>
     </div>
 
 </body>
