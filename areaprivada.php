2454b8e743c18d74cbeb862a87a9b1b664d5e711
Rayan-Aguiar <rayansiqueira@gmail.com>
2022-08-13 15:22:35 -0300

    Adicionado CSS na area privada

--- areaprivada.php
+++ areaprivada.php
@@ -18,7 +18,7 @@ if (!isset($_SESSION['ID'])) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style2.css">
+    <link rel="stylesheet" href="CSS/style2.css">
     <title>Area do cliente</title>
 </head>
 
@@ -30,14 +30,15 @@ if (!isset($_SESSION['ID'])) {
     $sql = $pdo->prepare("SELECT CPF, nome, email FROM cliente WHERE CPF = :id ");
     $sql->bindValue(":id", $id);
     $sql->execute();
-
     ?>
-
-    <h2>Seja bem-vindo(a)!</h2>
-    <div id="botton-exit">
-        <a href="#">Adicionar Saldo</a>
-        <a href="#">Transferir Saldo</a>
-        <a href="Sair.php">Sair</a>
+    
+    <div class="main-container">
+        <h2>Seja bem-vindo(a)!</h2>
+        <div class="bottons">
+            <a href="#">Adicionar Saldo</a>
+            <a href="#">Transferir Saldo</a>
+            <a href="Sair.php">Sair</a>
+        </div>
     </div>
 
 </body>
